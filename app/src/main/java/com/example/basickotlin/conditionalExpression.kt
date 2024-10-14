84004f2ee04409f868f4887f3d579255f8f9e1fa
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-14 09:43:21 +0700

    Write 'when' example in conditional expression

--- app/src/main/java/com/example/basickotlin/conditionalExpression.kt
+++ app/src/main/java/com/example/basickotlin/conditionalExpression.kt
@@ -14,4 +14,17 @@ fun conditionalStatement(){
     else{
         println("Negative Number")
     }
+
+    //when
+    val day = 1
+    when(day) {
+        1 ->  println("Monday")
+        2 ->  println("Tuesday")
+        3 ->  println("Wednesday")
+        4 ->  println("Thrusday")
+        5 ->  println("Payday")
+        6 ->  println("Saturday")
+        7 ->  println("Holiday")
+        else->println("Invalid")
+    }
 }
