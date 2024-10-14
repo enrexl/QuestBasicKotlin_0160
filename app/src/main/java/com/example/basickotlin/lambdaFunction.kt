1a816c77d63baa3b08942894077737770011dc84
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-14 10:17:52 +0700

    Write lambda function example in this case uppercase to file lambdaFunction.kt

--- app/src/main/java/com/example/basickotlin/lambdaFunction.kt
+++ app/src/main/java/com/example/basickotlin/lambdaFunction.kt
@@ -1,2 +1,17 @@
 package com.example.basickotlin
 
+//Lambda Function
+//Lambda funtions adalah fungsi yang tidak memiliki nama
+//Lambda function biasanya digunakan untuk membuat fungsi yang senderhana
+//Lambda functions menggunakan tanda pana (->) untuk memisahkan parameter dan body fungsi
+
+fun toUppercase(string : String) : String{
+    return string.uppercase()
+}
+
+//dapat ditulis dalam expressi lamba sbb:
+fun main(){
+    toUppercase("uhuy")
+    println({string:String -> string.uppercase()} ("uhuy"))
+    //UHUY
+}
