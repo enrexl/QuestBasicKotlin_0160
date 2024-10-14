8acfd48983c38ed30bfd40ed7582faae9f5b5263
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-14 09:29:59 +0700

    Write Set examples

--- app/src/main/java/com/example/basickotlin/collection.kt
+++ app/src/main/java/com/example/basickotlin/collection.kt
@@ -32,9 +32,40 @@ fun contohList(){
     // Mengambil string 'shape' dan menjadikannya readonly
     val shapesLock: List<String> = shape
     println(shapesLock)
-
 }
+
+// Set adalah Kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan set0f
+// Set Mutable menggunakan mutableSet0f
+
+fun contohSet(){
+    println("====== SET ======")
+
+    // readonly
+    val readOnlyAbjad = listOf('A','B','C')
+    println(readOnlyAbjad)
+
+    // Mutable (R/W)
+    val shape: MutableSet<String> = mutableSetOf("Square", "Hexa", "Triangle")
+    println(shape)
+
+    //--Add to Mutable list 'shape'
+    shape.add("Circle")
+    println(shape)
+
+    //--Remove from Mutale list 'shape'
+    shape.remove("Hexa")
+    println(shape)
+
+    // Mengambil string 'shape' dan menjadikannya readonly
+    val shapesLock: Set<String> = shape
+    println(shapesLock)
+}
+
 
 fun main(){
     contohList()
+    contohSet()
 }
