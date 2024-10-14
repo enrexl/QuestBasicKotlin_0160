1309fc78fe3cc4eab19e56df902236c138baa10f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-14 10:00:21 +0700

    Write dataclass examples in DataClass

--- app/src/main/java/com/example/basickotlin/DataClass.kt
+++ app/src/main/java/com/example/basickotlin/DataClass.kt
@@ -1,2 +1,34 @@
 package com.example.basickotlin
 
+//Data class
+//Data class adalah class yang digunakan untuk menyimpan dana
+//data class menyediakan fungsi untuk me-override fungsi equals(), hashCode(), dan toString()
+//Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+fun main(){
+    val data = DataClass(1,"rohman@gmail.com")
+    println(data)
+
+    //fungsi equals
+    val data2 = DataClass(2,"makan@gmail.com")
+    println(data == data2) //false
+
+    //fungsi copy
+    val data3 = data.copy()
+    println(data3)
+
+    //fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahara@umy.id")
+    println(data4)
+
+    println()
+    println(data)
+    println(data2)
+    println(data3)
+    println(data4)
+}
