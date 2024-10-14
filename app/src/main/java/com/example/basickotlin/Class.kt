09f338cb9eb4bccc52f965d8f7d1ccac32af43ae
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-14 09:09:49 +0700

    Menulis class dalam kotlin (id,email)

--- app/src/main/java/com/example/basickotlin/Class.kt
+++ app/src/main/java/com/example/basickotlin/Class.kt
@@ -1,2 +1,19 @@
 package com.example.basickotlin
 
+class Motor()
+
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+
+//Print line value dalam properti 'email'
+    println(contact.email)
+
+//Update value didalam property 'email'
+    contact.email = "Jane@Gmail.com"
+
+//Print line value baru dalam properti 'email'
+    println(contact.email)
+}
