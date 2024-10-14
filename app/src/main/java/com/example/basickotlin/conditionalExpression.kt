cccfbf7e217dadf90d6769661887924da0a6710f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-10-14 09:39:49 +0700

    Write if-else example in conditional expression

--- app/src/main/java/com/example/basickotlin/conditionalExpression.kt
+++ app/src/main/java/com/example/basickotlin/conditionalExpression.kt
@@ -1,2 +1,17 @@
 package com.example.basickotlin
 
+//ConditionalExpression adalah expresi yang mengembalkan nilai berdasarkan kondisi tertentu
+//ContiionalExpression menggunakan if-else, when, dan try-catch
+
+fun conditionalStatement(){
+    println("== Conditional Expression==")
+
+    //if-else
+    val number = 10
+    if (number>0){
+        println("Positive Number")
+    }
+    else{
+        println("Negative Number")
+    }
+}
